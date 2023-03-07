0e4db08cab3fbc1ac216715977e11eca46f2df39
Jeremiah Boby <mail@jeremiahboby.me>
2023-03-07 14:01:46 +0000

    Add django-configurations boilerplate

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -2,9 +2,13 @@
 
 import os
 
-from django.core.asgi import get_asgi_application
-
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "DummyConfiguration")
+
+import configurations
+
+configurations.setup()
+
+from django.core.asgi import get_asgi_application
 
 application = get_asgi_application()
