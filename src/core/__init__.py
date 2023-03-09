36897c4ecbe09496c09917f62f46b6d772a2bd52
Jeremiah Boby <mail@jeremiahboby.me>
2023-03-09 05:46:20 +0000

    Add DEFAULT_AUTHENTICATION_CLASSES

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -90,5 +90,9 @@ class BaseConfigurationMixin:
     REST_FRAMEWORK = {
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-        ]
+        ],
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+        ],
     }
