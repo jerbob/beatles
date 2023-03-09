b0c20631f7b60f28f2a908a18c4b91a52e761399
Jeremiah Boby <mail@jeremiahboby.me>
2023-03-09 04:41:55 +0000

    Add lyrics method to song viewset

--- src/songs/views.py
+++ src/songs/views.py
@@ -1,16 +1,40 @@
+from typing import Type, Union
+
 from rest_framework import serializers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from songs.models import Song
 
 
-class SongSerializer(serializers.HyperlinkedModelSerializer):
-    """A model serializer for beatles songs."""
+class FullSongSerializer(serializers.HyperlinkedModelSerializer):
+    """A song serializer that returns any relevant fields."""
 
     class Meta:
         model = Song
-        exclude = []
+        exclude = []  # Include all fields
+
+
+class SimpleSongSerializer(serializers.HyperlinkedModelSerializer):
+    """A song serializer that only returns specific fields."""
+
+    class Meta:
+        model = Song
+        fields = ["url", "name", "rank", "album", "writers"]
 
 
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
-    serializer_class = SongSerializer
+
+    def get_serializer_class(
+        self,
+    ) -> Union[Type[FullSongSerializer], Type[SimpleSongSerializer]]:
+        """Returns a different serializer dependent on authentication."""
+        if self.request.user.is_authenticated:
+            return FullSongSerializer
+        return SimpleSongSerializer
+
+    @action(detail=True, methods=["get"])
+    def lyrics(self, _, **__) -> Response:
+        song = self.get_object()
+        return Response({"lyrics": song.lyrics})
